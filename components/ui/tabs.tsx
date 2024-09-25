94fd781ea0fbcbd326ab6217ae6f051f1b53057e
Peko2r <wfhsyswd@gmail.com>
2024-09-25 20:25:42 +0800

    master

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,43 +1,55 @@
-import { useState } from "react";
-import ExploreCard, { ExploreCardProps } from "@/components/explore-card";
+"use client"
 
-export default function Tabs() {
-  const [activeTab, setActiveTab] = useState("all");
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
 
-  return (
-    <div className="nav nav-tabs align-items-center justify-content-center row">
-      <button className="nav-link active text-uppercase col-md-2">
-        <span className="icon">
-          <i className="mdi mdi-plus f-24 align-middle"></i> All
-        </span>
-      </button>
+import { cn } from "@/lib/utils"
 
-      <button className="nav-link text-uppercase col-md-2">
-        <span className="icon">
-          {" "}
-          <i className="mdi mdi-gamepad-variant f-24 align-middle"></i> Games
-        </span>
-      </button>
+const Tabs = TabsPrimitive.Root
 
-      <button className="nav-link text-uppercase col-md-2">
-        <span className="icon">
-          <i className="mdi mdi-brush f-24 align-middle"></i>
-          Art
-        </span>
-      </button>
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn(
+      "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+      className
+    )}
+    {...props}
+  />
+))
+TabsList.displayName = TabsPrimitive.List.displayName
 
-      <button className="nav-link text-uppercase col-md-2">
-        <span className="icon">
-          <i className="mdi mdi-panda f-24 align-middle"></i>
-          Memes
-        </span>
-      </button>
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow",
+      className
+    )}
+    {...props}
+  />
+))
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
-      <button className="nav-link text-uppercase col-md-2">
-        <span className="icon">
-          <i className="mdi mdi-collage f-24 align-middle"></i> Collection
-        </span>
-      </button>
-    </div>
-  );
-}
+const TabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn(
+      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      className
+    )}
+    {...props}
+  />
+))
+TabsContent.displayName = TabsPrimitive.Content.displayName
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
