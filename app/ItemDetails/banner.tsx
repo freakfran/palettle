788f5669bc6e3399f70be3b56b799d8d1e6b31f3
Peko2r <wfhsyswd@gmail.com>
2024-09-27 19:47:30 +0800

    master

---
+++ app/ItemDetails/banner.tsx
@@ -0,0 +1,58 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import {ConnectButton} from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+
+export default function Banner() {
+    return (
+        <NavigationMenu className="fixed p-2 max-w-full bg-white drop-shadow-sm">
+            <NavigationMenuList className="flex w-screen flex-row items-start justify-start">
+                <NavigationMenuItem className="flex-1 pl-20 cursor-pointer">
+                    <Link href="/public" legacyBehavior passHref>
+                        <Image src="/logo.png" alt='logo' width='50' height='50'/>
+                    </Link>
+                </NavigationMenuItem>
+                <div className="flex-1 flex justify-center space-x-4 pl-52">
+                    <NavigationMenuItem>
+                        <Link href="/public" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Home
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/arts" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                ArtWorks
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/rank" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Ranking
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/creators" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Creators
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </div>
+
+                <NavigationMenuItem className="flex-1 flex justify-end pr-20">
+                   <ConnectButton/>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
---
+++ app/ItemDetails/page.tsx
@@ -0,0 +1,5 @@
+function ItemDetails() {
+    return (
+        <div></div>
+  );
+}
